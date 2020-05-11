ea886accac7845792a04f224ef4761f68511a70c
mattcbodily <mattcbodily@gmail.com>
2020-05-11 08:52:47 -0600

    updated boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ThemeChanger from './Components/ThemeChanger';
 import RenderProp from './Components/RenderProp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ThemeChanger />
       {/* <RenderProp /> */}
     </div>
   );
