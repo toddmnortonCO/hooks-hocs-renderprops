c5f7eb5fe95a9ee3a82eb1fb4d72ae5f5d18bc07
mattcbodily <mattcbodily@gmail.com>
2020-05-11 08:00:36 -0600

    added boilerplate code

---
+++ src/Components/ThemeChanger.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class ThemeChanger extends Component {
+    render(){
+        return(
+            <div className='theme-changer'>ThemeChanger</div>
+        )
+    }
+}
